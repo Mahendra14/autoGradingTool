5bd88261c9442d4ff0923f79d841cc69e9c0765a
Raichur Mahendra Kumar <mahendra.raichur@gmail.com>
2023-08-12 16:51:47 -0700

    Adding required packages

--- autoGradingTool.js
+++ autoGradingTool.js
@@ -1,3 +1,12 @@
+const fs = require('fs');
+const { execSync } = require('child_process');
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
 // Main function
 function main() {
 
