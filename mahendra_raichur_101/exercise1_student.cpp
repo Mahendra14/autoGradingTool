06bc9933b15fa2361eb28ad588f9f6205bb88e19
Raichur Mahendra Kumar <mahendra.raichur@gmail.com>
2023-08-12 23:45:56 -0700

    adding an example for a student data stored

---
+++ mahendra_raichur_101/exercise1_student.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+// int addNumbers(int a, int b) {
+//   int sum = a + b;
+//   return sum;
+// }
+
+// int main() {
+//   int num1 = 2;
+//   int num2 = 5;
+
+//   int sum = addNumbers(num1, num2);
+
+//   cout << "The sum of " << num1 << " and " << num2 << " is: " << sum << endl;
+
+//   return 0;
+// }
+int main() {
+    int num1, num2;
+    std::cin >> num1num2;
+    int sum = num1 + num2;
+    std::cout << sum << std::endl;
+    return 0;
+}
